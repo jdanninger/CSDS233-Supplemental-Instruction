b273eabe886ff2d5ce1e16fa6f545806b5b86ee3
jdanninger <68190922+jdanninger@users.noreply.github.com>
2023-01-25 15:48:48 -0500

    started making jave method

---
+++ Session 1 - Intro to Recursion and Runtime Analysis/RecursivePractice.java
@@ -0,0 +1,10 @@
+public class RecursivePractice {
+
+    // Write your code below this point
+
+    // Write your code above this point
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+    }
+}
