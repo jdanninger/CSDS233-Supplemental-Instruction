ce2de367f244948673838418d632f5dfe8c04158
jdanninger <68190922+jdanninger@users.noreply.github.com>
2023-01-25 16:09:40 -0500

    answer key and test cases done!

--- Session 1 - Intro to Recursion and Runtime Analysis/RecursivePracticeAnswers.java
+++ Session 1 - Intro to Recursion and Runtime Analysis/RecursivePracticeAnswers.java
@@ -32,8 +32,25 @@ public class RecursivePracticeAnswers {
     // Write your code above this point *^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^
 
     public static void main(String[] args) {
-        // I am using the Main method for test cases. . . you do not need to mess with
-        // this :)
+        RecursivePracticeAnswers N = new RecursivePracticeAnswers();
+
+        System.out.println("The next few lines of console output should be 5, 4, 3, 2, 1");
+        N.countDown(5);
+
+        // I am using the Main method for test cases, you dont need to touch this
+        if (N.sumOfNumbers(5) == 15 && N.sumOfNumbers(100) == 5050) {
+            System.out.println("\n sumOfNumbers passes all test cases \n");
+        } else {
+            System.out.println("\n sumOfNumbers doesnt work :( \n");
+        }
+
+        int[] arr1 = { 1, 2, 4, 6, 8, 9 };
+        int[] arr2 = { 1, 2, 4, 60, 8, 9 };
+        if (N.isAscending(arr1, 1) && !N.isAscending(arr2, 1)) {
+            System.out.println("\n isAscending passes all test cases \n");
+        } else {
+            System.out.println("\n isAscending doesnt work :( \n");
+        }
 
     }
 }
