ca9eb792c7d8fa0c2506d9ee7e68ec0976f7bc1a
jdanninger <68190922+jdanninger@users.noreply.github.com>
2023-02-06 15:58:05 -0500

    refactor

--- Session 3 - Doubly Linked List in Depth/DoublyLinkedList.java
+++ Session 3 - Doubly Linked List in Depth/DoublyLinkedList.java
@@ -1,94 +1,90 @@
 import java.util.Arrays;
 
-public class DoublyLinkedList{
-    Node head;
-    Node tail; 
-    int size = 0;
-    private class Node {
+public class DoublyLinkedList {
+  Node head;
+  Node tail;
+  int size = 0;
 
-        int data;
-        Node next;
-        Node previous;
-        Node(int d) {
-          data = d;
-          next = null;
-          previous = null;
-        }
-      }
-      // WRTIE YOUR CODE BELOW *^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^
+  private class Node {
 
-      public void insert(int value, int index){
+    int data;
+    Node next;
+    Node previous;
 
-      }
+    Node(int d) {
+      data = d;
+      next = null;
+      previous = null;
+    }
+  }
+  // WRTIE YOUR CODE BELOW *^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^
 
-      public int[] intoArray(){
-        return null;
-      }
+  public void insert(int value, int index) {
 
+  }
 
+  public int[] intoArray() {
+    return null;
+  }
 
+  // WRTIE YOUR CODE Above *^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^
 
+  public void add(int insertMe) { // this method inserts a given int to end of LL
+    if (head == null) {
+      head = new Node(insertMe);
+      tail = head;
+    } else {
 
+      tail.next = new Node(insertMe); // set tail to point to newly inserted node
+      tail.next.previous = tail; // sets new node to point backwards to tail
+      tail = tail.next;
+    }
+    size++;
+  }
 
+  public void printList() { // this prints the LL out in console
+    String printMe = "";
+    Node currentNode = head;
+    while (currentNode != null) {
+      printMe = printMe + currentNode.data + ", ";
+      currentNode = currentNode.next;
+    }
+    System.out.println(printMe);
+  }
 
+  public int get(int index) {
+    Node itt = head;
+    for (int n = 0; n < index; n++) {
+      itt = itt.next;
+    }
+    return (itt.data);
+  }
 
-      // WRTIE YOUR CODE Above *^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^
+  public static void main(String[] args) {
+    DoublyLinkedList list = new DoublyLinkedList();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(5);
+    list.add(6);
+    list.add(7);
+    int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7 };
 
-      public void add(int insertMe) { //this method inserts a given int to end of LL
-        if (head == null) {
-          head = new Node(insertMe);
-          tail = head;
-        } else {
+    if (Arrays.equals(arr, list.intoArray())) {
+      System.out.println("intoArray Works!");
+    } else {
+      System.out.println("intoArray doesnt work");
+    }
 
-          tail.next = new Node(insertMe); //set tail to point to newly inserted node
-          tail.next.previous = tail;  //sets new node to point backwards to tail
-          tail = tail.next;
-        }
-        size++;
-      }
-      public void printList() { //this prints the LL out in console
-        String printMe = "";
-        Node currentNode = head;
-        while (currentNode != null) {
-          printMe = printMe + currentNode.data + ", ";
-          currentNode = currentNode.next;
-        }
-        System.out.println(printMe);
-      }
-      public int get(int index){
-        Node itt = head;
-        for(int n = 0; n<index; n++){
-          itt = itt.next;
-        }
-        return(itt.data);
-      }
+    list.insert(-1, 1);
+    list.insert(-2, 3);
+    if (list.get(1) == -1 && list.get(3) == -2) {
+      System.out.println("Insert Works!");
+    } else {
+      System.out.println("Insert doesnt work");
+    }
 
-      public static void main(String[] args){
-        DoublyLinkedList list = new DoublyLinkedList();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        list.add(6);
-        list.add(7);
-        int[] arr = new int[]{ 1,2,3,4,5,6,7 }; 
-
-        if(Arrays.equals(arr, list.intoArray())){
-          System.out.println("intoArray Works!");
-        }else{
-          System.out.println("intoArray doesnt work");
-        }
-
-        list.insert(-1,1);
-        list.insert(-2, 3);
-        if(list.get(1)==-1 && list.get(3)==-2){
-          System.out.println("Insert Works!");
-        }else{
-          System.out.println("Insert doesnt work");
-        }
-
-      }
-      
+  }
 
 }
